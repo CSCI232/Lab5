ef1ae9ac8422a213d0f46553f82908b9720ae50a
Gustavo <gustavo.barrionuevo@msu.montana.edu>
2014-09-27 17:11:31 -0600

    create Driver.java

---
+++ src/edu/montana/csci/Driver.java
@@ -0,0 +1,25 @@
+package edu.montana.csci;
+
+import java.util.ArrayList;
+
+public class Driver {
+	
+		public static void main(String[] args) {
+			ArrayList<Integer> listNodes = new ArrayList<Integer>();
+			listNodes.add(7);
+			listNodes.add(2);
+			listNodes.add(8);
+			listNodes.add(5);
+			listNodes.add(4);
+			Heap myHeap = new Heap(listNodes);
+			Visualizer tree = new Visualizer(myHeap);
+			tree.treeBuilder();
+			tree.viewGraph();
+			System.out.println("Input: " + listNodes.toString());
+			System.out.println("Heap: " + myHeap.getVertices().toString());
+			myHeap.heapSort();
+			System.out.print("HeapSort: " + myHeap.getVertices().toString());
+		}
+	}
+
+
